293aeb247efef856e1e696b7bbd49b6a34d1870b
Michael Perron <mikeperron.atx@gmail.com>
2022-03-07 17:35:06 -0600

    initial coommit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main (string[]args)
+        {
+            var stack = new Stack();
+            Console.WriteLine("Adding numbers");
+            Console.WriteLine("Printing numbers");
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            // stack.Push(4);
+            
+            stack.Display();
+
+            stack.Pop();
+            stack.Display();
+            // 
+        }
+    }
+}
---
+++ Stack.cs
@@ -0,0 +1,48 @@
+namespace Stack
+{
+    public class Stack
+    {
+        private readonly List<object> _list = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+            {
+                throw new InvalidOperationException("Null");
+            }
+            else
+            {
+                _list.Add(obj);
+            }
+        }
+
+        public void Display()
+        {
+
+            Console.WriteLine("List");
+            var li = _list;
+            for (int i = 0; i < li.Count; i ++)
+            {
+                Console.WriteLine("Element: " + li[i]);
+            }
+           // _list.ForEach(Console.WriteLine);
+        }
+
+        public void Pop()
+        {
+            if(_list.Count == 0)
+            {
+                throw new InvalidOperationException("None in Stack");
+            }
+            else
+            {
+                _list.RemoveAt(_list.Count - 1);
+            }
+        }
+
+        // void Clear()
+        // {
+
+        // }
+    }
+}
